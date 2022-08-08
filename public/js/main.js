ce48aed54233ddf12a986335e353d2e88aa10e6b
Darkshadows61 <bisomt@gmail.com>
2022-08-08 18:07:00 -0400

    added method to move a game from backlog to complete that also deletes from backlog

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,14 @@
 //Constants
-const deleteTomText = document.querySelectorAll('#tom-trash')
-const thumbTomText = document.querySelectorAll('#tom-thumbs-up')
-const deleteStephenText = document.querySelectorAll('#stephen-trash')
-const thumbStephenText = document.querySelectorAll('#stephen-thumbs-up')
+//Tom
+    const deleteTomText = document.querySelectorAll('#tom-trash')
+    const thumbTomText = document.querySelectorAll('#tom-thumbs-up')
+    const finishTomText = document.querySelectorAll('#tom-finish')
+    const finishTomDelete = document.querySelectorAll('#tom-trash-finish')
+//Stephen
+    const deleteStephenText = document.querySelectorAll('#stephen-trash')
+    const thumbStephenText = document.querySelectorAll('#stephen-thumbs-up')
+    const finishStephenText = document.querySelectorAll('#stephen-finish')
+    const finishStephenDelete = document.querySelectorAll('#stephen-trash-finish')
 
 //Tom Array
 Array.from(deleteTomText).forEach((element)=>{
@@ -13,12 +19,42 @@ Array.from(thumbTomText).forEach((element)=>{
     element.addEventListener('click', addOneTomLike)
 })
 
+Array.from(finishTomText).forEach((element) =>{
+    element.addEventListener('click', finishTomGame)
+    element.addEventListener('click', deleteTomGame)
+})
+
+Array.from(finishTomDelete).forEach((element) =>{
+    element.addEventListener('click', deleteTomGameFinish)
+})
+
 //Tom async await
 async function deleteTomGame(){
     const name = this.parentNode.childNodes[1].innerText
     const platform = this.parentNode.childNodes[3].innerText
     try{
         const res = await fetch('deleteTomGame', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'name': name,
+              'platform': platform
+            })
+          })
+        const data = await res.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function deleteTomGameFinish(){
+    const name = this.parentNode.childNodes[1].innerText
+    const platform = this.parentNode.childNodes[3].innerText
+    try{
+        const res = await fetch('deleteTomGameFinish', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -58,6 +94,30 @@ async function addOneTomLike(){
     }
 }
 
+async function finishTomGame(){
+    const name = this.parentNode.childNodes[1].innerText
+    const platform = this.parentNode.childNodes[3].innerText
+    const tLikes = Number(this.parentNode.childNodes[5].innerText)
+    try{
+        const res = await fetch('finishTomGame', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'name': name,
+              'platform': platform,
+              'likes': tLikes
+            })
+        })
+        const data = await res.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
 //Stephen Array
 Array.from(deleteStephenText).forEach((element)=>{
     element.addEventListener('click', deleteStephenGame)
@@ -67,12 +127,42 @@ Array.from(thumbStephenText).forEach((element)=>{
     element.addEventListener('click', addOneStephenLike)
 })
 
+Array.from(finishStephenText).forEach((element) =>{
+    element.addEventListener('click', finishStephenGame)
+    element.addEventListener('click', deleteStephenGame)
+})
+
+Array.from(finishStephenDelete).forEach((element) =>{
+    element.addEventListener('click', deleteStephenGameFinish)
+})
+
 //Stephen Async Await
 async function deleteStephenGame(){
     const name = this.parentNode.childNodes[1].innerText
     const platform = this.parentNode.childNodes[3].innerText
     try{
         const res = await fetch('deleteStephenGame', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'name': name,
+              'platform': platform
+            })
+          })
+        const data = await res.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function deleteStephenGameFinish(){
+    const name = this.parentNode.childNodes[1].innerText
+    const platform = this.parentNode.childNodes[3].innerText
+    try{
+        const res = await fetch('deleteStephenGameFinish', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -103,6 +193,29 @@ async function addOneStephenLike(){
               'likes': tLikes
             })
           })
+        const data = await res.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function finishStephenGame(){
+    const name = this.parentNode.childNodes[1].innerText
+    const platform = this.parentNode.childNodes[3].innerText
+    const tLikes = Number(this.parentNode.childNodes[5].innerText)
+    try{
+        const res = await fetch('finishStephenGame', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'name': name,
+              'platform': platform,
+              'likes': tLikes
+            })
+        })
         const data = await res.json()
         console.log(data)
         location.reload()
--- server.js
+++ server.js
@@ -29,16 +29,22 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
         console.log('Connection to MongoDB Complete. Ready to game')
         const db = client.db(dbName)//Database Name
         const tomBacklog = db.collection('tomBacklog')
+        const tomBacklogComplete = db.collection('tomBacklogComplete')
         const stephenBacklog = db.collection('stephenBacklog')
+        const stephenBacklogComplete = db.collection('stephenBacklogComplete')
 
         app.get('/', (req,res)=>{
             tomBacklog.find().toArray().then(result=>{
                 stephenBacklog.find().toArray().then(results =>{
-                  res.render('index.ejs', { tomBacklog: result, stephenBacklog: results})
+                    tomBacklogComplete.find().toArray().then(resultC => {
+                        stephenBacklogComplete.find().toArray().then(resultS =>{
+                            res.render('index.ejs', { tomBacklog: result, stephenBacklog: results, tomBacklogComplete: resultC, stephenBacklogComplete: resultS})    
+                        })
+                    })
                 })
             .catch(error => console.error(error))
         })
-
+        //Tom Add Game
         app.post('/addTomGame', (req,res) =>{
             tomBacklog.insertOne({name: req.body.name, platform: req.body.platform, likes: 0})
             .then(result => {
@@ -47,7 +53,16 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
             })
            .catch(error => console.error(error))
         })
-
+        //Tom Finish Game
+        app.post('/finishTomGame', (req,res) =>{
+            tomBacklogComplete.insertOne({name: req.body.name, platform: req.body.platform, likes: 0})
+            .then(result =>{
+                console.log('Tom finished a game! Moved to Completed')
+                res.redirect('/')
+            })
+            .catch(error => console.error(error))
+        })
+        //Stephen Add Game        
         app.post('/addStephenGame', (req,res) =>{
             stephenBacklog.insertOne({name: req.body.name, platform: req.body.platform, likes: 0})
             .then(result => {
@@ -56,7 +71,16 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
             })
            .catch(error => console.error(error))
         })
-
+        //Stephen Finish Game
+        app.post('/finishStephenGame', (req,res) =>{
+            stephenBacklogComplete.insertOne({name: req.body.name, platform: req.body.platform, likes: 0})
+            .then(result =>{
+                console.log('Stephen finished a game! Moved to Completed')
+                res.redirect('/')
+            })
+            .catch(error => console.error(error))
+        })
+        //Tom Add one Like
         app.put('/addOneTomLike', (req,res)=>{
             tomBacklog.updateOne({name: req.body.name, platform: req.body.platform, likes: req.body.likes},{
                 $set: {
@@ -72,7 +96,7 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
             })
             .catch(error => console.error(error))
         })
-
+        //Stephen Add One Like
         app.put('/addOneStephenLike', (req,res)=>{
             stephenBacklog.updateOne({name: req.body.name, platform: req.body.platform, likes: req.body.likes},{
                 $set: {
@@ -98,11 +122,29 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
             .catch(error => console.error(error))
         })
 
+        app.delete('/deleteTomGameFinish', (req, res) => {
+            tomBacklogComplete.deleteOne({name: req.body.name})
+            .then(result => {
+                console.log('Tom Game F Deleted')
+                res.json('Tom Game F Deleted')
+            })
+            .catch(error => console.error(error))
+        })
+
         app.delete('/deleteStephenGame', (req, res) => {
             stephenBacklog.deleteOne({name: req.body.name})
             .then(result => {
                 console.log('Stephen Game Deleted')
                 res.json('Stephen Game Deleted')
+            })
+            .catch(error => console.error(error))
+        })
+
+        app.delete('/deleteStephenGameFinish', (req, res) => {
+            stephenBacklogComplete.deleteOne({name: req.body.name})
+            .then(result => {
+                console.log('Stephen Game F Deleted')
+                res.json('Stephen Game F Deleted')
             })
             .catch(error => console.error(error))
         })
